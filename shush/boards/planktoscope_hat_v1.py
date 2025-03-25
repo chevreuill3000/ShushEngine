d52c8df5c568405f8439ca69f56dfcc64d29b826
chevreuill3000 <thibaut+github@fairscope.com>
2025-03-25 12:22:46 +0100

    Create planktoscope_hat_v1.py
    Create a pin out specific to the PlanktoScope Hat

---
+++ shush/boards/planktoscope_hat_v1.py
@@ -0,0 +1,9 @@
+# Define SPI Chip Select pin for each motor using RPi GPIO
+# Define enable pins to enable/disable motor
+
+m0_cs = 27
+m0_enable = 23
+m1_cs = 17
+m1_enable = 5
+error = 16
+stall = 20
